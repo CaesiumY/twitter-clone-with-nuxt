02f6689462eae19057af206ace04460f0032e3a5
caesiumy <mn0658@naver.com>
2020-10-25 20:11:04 +0900

    feat: Add Profile page frames only

---
+++ front/components/FollowList.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-list>
+    <v-list-item>
+      <span>오렌지</span>
+      <v-icon>
+        mdi-minus-circle-outline
+      </v-icon>
+    </v-list-item>
+    <v-list-item>
+      <span>오렌지</span>
+      <v-icon>
+        mdi-minus-circle-outline
+      </v-icon>
+    </v-list-item>
+    <v-list-item>
+      <span>오렌지</span>
+      <v-icon>
+        mdi-minus-circle-outline
+      </v-icon>
+    </v-list-item>
+  </v-list>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
--- front/pages/Profile.vue
+++ front/pages/Profile.vue
@@ -1,13 +1,47 @@
 <template>
-  <div>
-    profile
-  </div>
+  <v-container>
+    <v-card>
+      <v-container>
+        <v-subheader>
+          내 프로필
+        </v-subheader>
+        <v-form>
+          <v-text-field label="닉네임" required />
+          <v-btn type="submit" dark color="blue">
+            수정
+          </v-btn>
+        </v-form>
+      </v-container>
+    </v-card>
+
+    <v-card class="mt-3">
+      <v-container>
+        <v-subheader>
+          Follows
+        </v-subheader>
+        <follow-list></follow-list>
+      </v-container>
+    </v-card>
+    <v-card class="mt-3">
+      <v-container>
+        <v-subheader>
+          Followers
+        </v-subheader>
+        <follow-list></follow-list>
+      </v-container>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
+import FollowList from "../components/FollowList";
+
 export default {
   head: {
     title: "Profile",
+  },
+  components: {
+    FollowList,
   },
 };
 </script>
