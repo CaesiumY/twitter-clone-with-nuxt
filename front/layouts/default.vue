7d6d8abaa83516adde71bbd53380073d7f35b41a
caesiumy <mn0658@naver.com>
2020-10-25 16:16:33 +0900

    feat: Add vuetify navbar

--- front/layouts/default.vue
+++ front/layouts/default.vue
@@ -1,16 +1,46 @@
 <template>
-  <div>
-    <nav>
-      <nuxt-link to="/">Home</nuxt-link>
-      <nuxt-link to="/profile">Profile</nuxt-link>
-      <nuxt-link to="/signup">SignUp</nuxt-link>
-    </nav>
-    <Nuxt />
-  </div>
+  <v-app>
+    <div>
+      <v-toolbar dark color="green">
+        <v-toolbar-title>
+          <nuxt-link to="/" class="d-flex align-center">
+            <v-icon class="mr-2">
+              mdi-twitter
+            </v-icon>
+            Nuxtwitter
+          </nuxt-link>
+        </v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-toolbar-items>
+          <v-text-field
+            label="검색"
+            hide-details
+            prepend-icon="mdi-magnify"
+            class="d-flex align-center"
+          />
+
+          <v-btn class="d-flex align-center" text nuxt to="/profile">
+            <div>프로필</div>
+          </v-btn>
+          <v-btn class="d-flex align-center" text nuxt to="/signup">
+            <div>회원가입</div>
+          </v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+    </div>
+  </v-app>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style scoped></style>
+<style scoped>
+a {
+  display: inline-block;
+  text-decoration: none;
+  color: inherit;
+}
+</style>
